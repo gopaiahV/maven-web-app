163c528ec9b0b787477c2bcee45d1e1cd9b0e2ef
gopaiah v <114583990+gopaiahV@users.noreply.github.com>
2025-08-05 00:28:12 +0530

    Update EmployeeService.java
    updated source code

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -1,8 +1,8 @@
 package com.mt.services;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,19 +15,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/employee")
 public class EmployeeService {
 
-	
-	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
-	@ResponseBody
-	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-			throws JSONException {
+    @RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+    @ResponseBody
+    public String getEmployeeDetails(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+            throws JSONException {
 
-		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+        JSONObject js = new JSONObject();
+        js.put("Name", "Mithun Technologies");
+        js.put("Calling Name", "Mithun");
+        js.put("DOB", "08-Nov-2011");
+        js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+        js.put("Places he like", "His native place");
 
-		return js.toString();
-}
+        return js.toString();
+    }
 }
